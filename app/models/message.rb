d688d5dacd92f7687c7d0a74551030af410da819
takkun5 <takuya0215sai@gmail.com>
2015-07-24 12:01:00 +0000

    age edit validation

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,6 +2,7 @@ class Message < ActiveRecord::Base
     
     validates :name, length: { maximum: 20 }, presence: true
     # 内容は必須入力かつ2文字以上30文字以下
-    validates :age, length: { maximum: 2}, numericality: true, presence: true
+    validates :age, length: { maximum: 2}, numericality: { only_integer: true, greater_than_or_equal_to: 0 },
+    presence: true
     validates :body , length: { minimum: 2 , maximum: 30 } , presence: true
 end
